75872a310972bd59398946e063482eaafb622109
Julian Hagenauer <julian.hagenauer@daimler.com>
2018-09-14 14:30:14 +0200

    Deleted unnecessary files for supervised learning.

--- src/main/java/spawnn/SupervisedNet.java
+++
@@ -1,7 +0,0 @@
-package spawnn;
-
-public interface SupervisedNet {
-	public double[] present( double[] x );
-	public double[] getResponse( double[] x, double[] neuron );
-	public void train( double t, double[] x, double[] desired ); //Problem: RBF is not time-dependend
-}
